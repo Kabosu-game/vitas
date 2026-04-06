6a663582079fb8c1815293c7d7333c2943a950cd
Kabosu <kbosugame@gmail.com>
2026-04-06 10:55:38 +0100

    Fix: remove DB mail override from SettingServiceProvider - use .env directly

--- app/Providers/SettingServiceProvider.php
+++ app/Providers/SettingServiceProvider.php
@@ -25,24 +25,6 @@ class SettingServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (App::dbConnectionCheck() && Schema::hasTable('settings')) {
-
-            $mailHost     = setting('mail_host', 'mail');
-            $mailPort     = setting('mail_port', 'mail');
-            $mailUsername = setting('mail_username', 'mail');
-            $mailPassword = setting('mail_password', 'mail');
-            $mailSecure   = setting('mail_secure', 'mail');
-            $fromName     = setting('email_from_name', 'mail');
-            $fromAddress  = setting('email_from_address', 'mail');
-
-            if (!empty($mailHost))     config()->set('mail.mailers.smtp.host',       $mailHost);
-            if (!empty($mailPort))     config()->set('mail.mailers.smtp.port',       $mailPort);
-            if (!empty($mailUsername)) config()->set('mail.mailers.smtp.username',   $mailUsername);
-            if (!empty($mailPassword)) config()->set('mail.mailers.smtp.password',   $mailPassword);
-            if (!empty($mailSecure))   config()->set('mail.mailers.smtp.encryption', $mailSecure);
-            if (!empty($fromName))     config()->set('mail.from.name',               $fromName);
-            if (!empty($fromAddress))  config()->set('mail.from.address',            $fromAddress);
-
-        }
+        // Mail config is read directly from .env (MAIL_HOST, MAIL_PORT, etc.)
     }
 }
